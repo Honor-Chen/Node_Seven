5ee856da22c5367adebb8592bbb5c372efa06ff3
clhonor <clhonor@163.com>
2023-02-16 19:47:16 +0800

    ✨ feat(tools.js): 大驼峰命名法

--- tools.js
+++ tools.js
@@ -33,11 +33,11 @@ export const SRC = (...args) => resolve.apply(null, [__dirname, './', 'src']).co
 		]
 */
 export const getDirectory = (base) =>
-  readdirSync(base).reduce((prev, cur) => {
-    const item = join(base, cur)
-    const stat = statSync(item)
-    return stat.isDirectory() && prev.push({ cur, item }), prev // !：注意：一行解决返回值问题
-  }, [])
+    readdirSync(base).reduce((prev, cur) => {
+        const item = join(base, cur)
+        const stat = statSync(item)
+        return stat.isDirectory() && prev.push({ cur, item }), prev // !：注意：一行解决返回值问题
+    }, [])
 
 /*
 	!获取指定文件夹下一级目录 | 文件
@@ -48,37 +48,52 @@ export const getDirectory = (base) =>
 		]
 */
 export const getFileOfFilter = (filePath, fileFilter) => {
-  if (!existsSync(filePath)) return null
+    if (!existsSync(filePath)) return null
 
-  return readdirSync(filePath)
-    .filter(fileFilter || (() => true))
-    .map((file) => join(filePath, file))
+    return readdirSync(filePath)
+        .filter(fileFilter || (() => true))
+        .map((file) => join(filePath, file))
 }
 
 /*
 	!key 和 value 相互转换
  */
 export const changeKeyValue = () => {
-  const loaders = {
-    json: ['.json', '.geojson'],
-    file: ['.png', '.jpg', '.webp'],
-  }
-  return Object.entries(loaders).reduce((prev, [type, exts]) => Object.assign(prev, ...exts.map((ext) => ({ [ext]: type }))), {})
+    const loaders = {
+        json: ['.json', '.geojson'],
+        file: ['.png', '.jpg', '.webp'],
+    }
+    return Object.entries(loaders).reduce((prev, [type, exts]) => Object.assign(prev, ...exts.map((ext) => ({ [ext]: type }))), {})
 }
 
 /*
 	!别名插件中的查找文件的方法
  */
 export const detectFile = async (importPath) => {
-  try {
-    const stats = await stat(importPath)
-    if (stats.isDirectory()) {
-      return join(importPath, 'index.js')
+    try {
+        const stats = await stat(importPath)
+        if (stats.isDirectory()) {
+            return join(importPath, 'index.js')
+        }
+        return importPath
+    } catch (ex) {
+        return `${importPath}.js`
     }
-    return importPath
-  } catch (ex) {
-    return `${importPath}.js`
-  }
 }
 
-console.log(changeKeyValue())
+/*
+	!将文件名称 test-demo 改为 TestDemo 大驼峰命名法
+ */
+export const camelCaseString = (str = '') => {
+    if (str.includes('-')) {
+        let tempStr = str.toLowerCase().split('-')
+
+        for (let i = 0; i < tempStr.length; i++) {
+            tempStr[i] = tempStr[i].substring(0, 1).toUpperCase() + tempStr[i].substring(1)
+        }
+        return tempStr.join('')
+    }
+    return str
+}
+
+console.log(camelCaseString('color-demo'))
